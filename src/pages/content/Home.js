c072f654ea5f1c8136cfa958e2d2d7a6fbd99b88
ice-bear <andydowsen47us@gmail.com>
2023-02-18 12:37:36 +0530

    Home page designing started

--- src/pages/content/Home.js
+++ src/pages/content/Home.js
@@ -1,15 +1,69 @@
 import React from "react";
-import home from "./scss/Home.module.css";
-import { Container } from "@nextui-org/react";
+import { Button } from "@material-tailwind/react";
+import { Text, Button as NextUiButton } from "@nextui-org/react";
 
 const HomePage = () => {
   return (
     <>
-    <div className={`container ${home.content}`}>
-      <Container className={`container ${home.content}`}>
-        <h1 className={`${home.heading}`}>The home of the innovators of web</h1>
-      </Container>
-    </div>
+        <div className="relative px-4 lg:px-8">
+          <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+            <svg
+              className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+              viewBox="0 0 1155 678"
+            >
+              <path
+                fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
+                fillOpacity=".3"
+                d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+              />
+              <defs>
+                <linearGradient
+                  id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
+                  x1="1155.49"
+                  x2="-78.208"
+                  y1=".177"
+                  y2="474.645"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#9089FC" />
+                  <stop offset={1} stopColor="#FF80B5" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </div>
+
+          <div className="mx-auto max-w-2xl py-16 sm:py-48 lg:py-56">
+            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+              <div className="relative rounded-full py-1 px-3 text-sm leading-3 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                Announcing our next round of funding.{" "}
+                <a href="#" className="font-semibold text-rose-600">
+                  <span className="absolute inset-0" aria-hidden="true" />
+                  Read more <span aria-hidden="true">&rarr;</span>
+                </a>
+              </div>
+            </div>
+            <div className="text-center">
+              <Text
+                h1
+                className="text-6xl  font-bold tracking-tight sm:text-6xl"
+              >
+                Data to enrich your online business
+              </Text>
+              <Text
+                p
+                className="font-poppins mt-6 text-lg leading-8 text-gray-600"
+              >
+                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+                fugiat aliqua.
+              </Text>
+              <div className="mt-10 flex items-center justify-center gap-x-6">
+                <Button ripple={true}>Learn more</Button>
+                <Button ripple={true}>Ripple Effect On</Button>
+              </div>
+            </div>
+          </div>
+        </div>
     </>
   );
 };
